f713e81256b741af564db0e82e1ebdd9024f58a0
sujith savithry balan <sujithma1992@gmail.com>
2018-08-09 23:53:25 +0530

    transactions list component created

--- src/app/transactions-list/transactions-list.module.ts
+++ src/app/transactions-list/transactions-list.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+import { TransactionsListComponent } from './transactions-list.component';
+
+
+const routes: Routes = [
+	{
+		path: '', component: TransactionsListComponent
+	}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: []
+  declarations: [TransactionsListComponent]
 })
 export class TransactionsListModule { }
